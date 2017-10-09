f95c793ec6151ad4a0b0640e6ad1720f2added26
Prashanth Menon <pmenon@cs.cmu.edu>
2017-10-09 14:31:43 -0400

    Use locked_table interface to elide locks

--- hash-table-shootout/src/libcuckoo_map.cc
+++ hash-table-shootout/src/libcuckoo_map.cc
@@ -7,35 +7,37 @@
 typedef cuckoohash_map<KeyType, ValueType, Hasher> hash_t;
 typedef cuckoohash_map<std::string, std::int64_t> str_hash_t;
 
-#define SETUP  \
-  hash_t hash; \
-  hash_t str_hash;
+#define SETUP                                       \
+  hash_t mt_hash;                                   \
+  hash_t::locked_table hash = mt_hash.lock_table(); \
+  hash_t mt_str_hash;                               \
+  hash_t::locked_table str_hash = mt_str_hash.lock_table();
 
 #define RESERVE_INT(size) hash.reserve(size)
 #define RESERVE_STR(size) str_hash.reserve(size)
 #define LOAD_FACTOR(map) map.load_factor()
 
 #define INSERT_INT_INTO_HASH(key, value) \
-  if (!hash.insert(key, value)) {        \
+  auto ret = hash.insert(key, value);    \
+  if (!ret.second) {                     \
     printf("error on insert");           \
     exit(1);                             \
   }
+
 #define DELETE_INT_FROM_HASH(key)
 #define FIND_INT_EXISTING_FROM_HASH(key) \
-  ValueType v;                           \
-  if (!hash.find(key, v)) {              \
+  if (hash.find(key) == hash.end()) {    \
     printf("error");                     \
     exit(1);                             \
   }
 #define FIND_INT_MISSING_FROM_HASH(key) \
-  ValueType v;                          \
-  if (hash.find(key, v)) {              \
+  if (hash.find(key) != hash.end()) {   \
     printf("error");                    \
     exit(2);                            \
   }
 #define FIND_INT_EXISTING_FROM_HASH_COUNT(key, count) \
   ValueType v;                                        \
-  if (hash.find(key)) {                               \
+  if (hash.find(key) != hash.end()) {                 \
     count++;                                          \
   }
 #define CHECK_INT_ITERATOR_VALUE(iterator, value)
