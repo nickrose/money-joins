187255ff4ca84216ebbcc65c214428171f409155
Tony Seo <seojungmin@gmail.com>
2017-10-18 14:24:56 -0400

    working version of string

--- hash-table-shootout/src/libcuckoo_map.cc
+++ hash-table-shootout/src/libcuckoo_map.cc
@@ -5,17 +5,18 @@
 #include "common.h"
 
 typedef cuckoohash_map<KeyType, ValueType, Hasher> hash_t;
-typedef cuckoohash_map<std::string, std::int64_t> str_hash_t;
+typedef cuckoohash_map<std::string, ValueType, std::hash<std::string>> str_hash_t;
 
 #define SETUP                                       \
   hash_t mt_hash;                                   \
   hash_t::locked_table hash = mt_hash.lock_table(); \
-  hash_t mt_str_hash;                               \
-  hash_t::locked_table str_hash = mt_str_hash.lock_table();
+  str_hash_t mt_str_hash;                               \
+  str_hash_t::locked_table str_hash = mt_str_hash.lock_table();
 
 #define RESERVE_INT(size) hash.reserve(size)
 #define RESERVE_STR(size) str_hash.reserve(size)
-#define LOAD_FACTOR(map) map.load_factor()
+#define LOAD_FACTOR_HASH() hash.load_factor()
+#define LOAD_FACTOR_STR_HASH() str_hash.load_factor()
 
 #define INSERT_INT_INTO_HASH(key, value) \
   auto ret = hash.insert(key, value);    \
@@ -42,10 +43,28 @@ typedef cuckoohash_map<std::string, std::int64_t> str_hash_t;
   }
 #define CHECK_INT_ITERATOR_VALUE(iterator, value)
 
-#define INSERT_STR_INTO_HASH(key, value)
+#define INSERT_STR_INTO_HASH(key, value) \
+  auto ret = str_hash.insert(key, value);    \
+  if (!ret.second) {                     \
+    printf("error on insert");           \
+    exit(1);                             \
+  }
+
 #define DELETE_STR_FROM_HASH(key)
-#define FIND_STR_EXISTING_FROM_HASH(key)
-#define FIND_STR_MISSING_FROM_HASH(key)
-#define FIND_STR_EXISTING_FROM_HASH_COUNT(key, count)
+#define FIND_STR_EXISTING_FROM_HASH(key) \
+  if (str_hash.find(key) == str_hash.end()) {    \
+    printf("error");                     \
+    exit(1);                             \
+  }
+#define FIND_STR_MISSING_FROM_HASH(key) \
+  if (str_hash.find(key) != str_hash.end()) {   \
+    printf("error");                    \
+    exit(2);                            \
+  }
+#define FIND_STR_EXISTING_FROM_HASH_COUNT(key, count) \
+  ValueType v;                                        \
+  if (str_hash.find(key) != str_hash.end()) {                 \
+    count++;                                          \
+  }
 
 #include "template.c"
